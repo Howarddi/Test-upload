0f11c8120b71cc270ffb8adaaedb20e6d743e00b
H1R0L9 <harold.indra@gmail.com>
2020-11-21 12:04:37 +0700

    Added movement controls, car model, and camera

---
+++ Scripts/car.gd
@@ -0,0 +1,26 @@
+extends KinematicBody
+
+#Declare member variables here
+export var gravity = Vector3.DOWN * 50
+export var speed = 100
+export var rot_speed = 3.5
+
+var velocity = Vector3.ZERO
+
+func _physics_process(delta):
+	velocity += gravity * delta
+	get_input(delta)
+	velocity = move_and_slide(velocity, Vector3.UP)
+
+func get_input(delta):
+	var vy = velocity.y
+	velocity = Vector3.ZERO
+	if Input.is_action_pressed("forward"):
+		velocity += -transform.basis.x * speed
+	if Input.is_action_pressed("back"):
+		velocity += transform.basis.x * speed
+	if Input.is_action_pressed("right"):
+		rotate_y(-rot_speed * delta)
+	if Input.is_action_pressed("left"):
+		rotate_y(rot_speed * delta)
+	velocity.y = vy
