18e24b38418e9322dc9681eec5b9129b41b2e6df
Ricky Halim Putra <rickyputra@awantunai.com>
2020-11-28 15:10:29 +0800

    align with surface

--- Scripts/mini_tank.gd
+++ Scripts/mini_tank.gd
@@ -1,23 +1,19 @@
 extends KinematicBody
 
-#Declare member variables here
-export var gravity = Vector3.DOWN * 50
-export var speed = 10
-export var rot_speed = 3.5
-
+export var gravity = Vector3.DOWN * 10
+export var speed = 8
+export var rot_speed = 0.85
 var velocity = Vector3.ZERO
 
 func align_with_y(xform, new_y):
 	xform.basis.y = new_y
 	xform.basis.x = -xform.basis.z.cross(new_y)
 	xform.basis = xform.basis.orthonormalized()
-
 	return xform
 	
 func _physics_process(delta):
 	velocity += gravity * delta
 	get_input(delta)
-	#velocity = move_and_slide(velocity, Vector3.UP)
 	velocity = move_and_slide_with_snap(velocity, Vector3.DOWN*2, Vector3.UP, true)
 	#for i in get_slide_count():
 		#var c = get_slide_collision(i)
@@ -27,19 +23,14 @@ func _physics_process(delta):
 	global_transform = global_transform.interpolate_with(xform, 0.2)
 
 func get_input(delta):
-	var vy = velocity.y
-	velocity = Vector3.ZERO
-	if Input.is_action_pressed("forward"):
-		velocity += -transform.basis.x * speed
-	if Input.is_action_pressed("back"):
-		velocity += transform.basis.x * speed
-	if Input.is_action_pressed("right"):
+	velocity = Vector3()
+	if Input.is_action_pressed(("forward")):
+		velocity += transform.basis.z * speed
+	if Input.is_action_pressed(("back")):
+		velocity += -transform.basis.z * speed
+	if Input.is_action_pressed(("right")):
+		#velocity += -transform.basis.x * speed
 		rotate_y(-rot_speed * delta)
-	if Input.is_action_pressed("left"):
+	if Input.is_action_pressed(("left")):
+		#velocity += transform.basis.x * speed
 		rotate_y(rot_speed * delta)
-	velocity.y = vy
-
-
-func _on_coin_coinCollected():
-	print("coin collected")
-	speed += 5
